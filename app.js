50aec0034726a90f93f2ec2d678a11a5038c4131
Shankhadeep Bhadra <shankhadeepraj@gmail.com>
2019-12-07 09:23:45 +0530

    added user input funtionality

---
+++ app.js
@@ -0,0 +1,9 @@
+const validator = require("validator");
+const chalk = require("chalk");
+const getNotes = require("./notes");
+
+const command = process.argv[2];
+
+if (command === "add") {
+  console.log("adding note");
+}
